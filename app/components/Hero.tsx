87318e67b80aa11c4a37275c1cafc202d16acf74
Ethan <ecala420@gmail.com>
2025-01-28 20:41:15 -0600

    attempt to fix image issue again

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import { motion } from "framer-motion"
 import { Button } from "@/components/ui/button"
-import profilePic from "@/public/me.jpg";
+import profilePic from "@/public/assets/me.png";
 
 export default function Hero() {
 
