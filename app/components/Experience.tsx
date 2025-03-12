e9c03432ade26df4891813712aa38e9216aed83f
Ethan Cala <156372497+ethancala@users.noreply.github.com>
2025-03-11 23:22:14 -0500

    Update Experience.tsx

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -7,9 +7,7 @@ import { Button } from "@/components/ui/button"
 const experiences = [
   {
     title: "IT intern | Sidley Austin LLP",
-    description: "IT Engineering: Developed Python Selenium testing applications to monitor, maintain and test IT infrastructure. Developed Python, Powershell, and bash scripts to automate tasks that drastically improved productivity. Imaged and deployed machines for users. Performed Data migrations for users, while maintaining system availability. Wiped 100+ physical machine's data by deploying scripts that comply with department of defense standards. Monitor System trending incidents and executed solutions apart of a team.  Contributed to the new hire onboard process by providing seamless tech integration. 
-
-Big Law Firm Experience",
+    description: "IT Engineering: Developed Python Selenium testing applications to monitor, maintain and test IT infrastructure. Developed Python, Powershell, and bash scripts to automate tasks that drastically improved productivity. Imaged and deployed machines for users. Performed Data migrations for users, while maintaining system availability. Wiped 100+ physical machine's data by deploying scripts that comply with department of defense standards. Monitor System trending incidents and executed solutions apart of a team.  Contributed to the new hire onboard process by providing seamless tech integration. Big Law Firm Experience",
     date: "2024-02-27 to Present" ,
     website: "https://www.sidley.com/en/us/"
   },
